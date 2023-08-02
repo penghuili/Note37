4651cc02cbc9cf626482c20c6742a7618fe268e1
Peng <1a84@proton.me>
2023-08-02 10:38:03 +0200

    remove payment status

--- src/views/Account/Account.js
+++ src/views/Account/Account.js
@@ -1,7 +1,6 @@
 import { Anchor, Spinner, Text } from 'grommet';
 import React from 'react';
 
-import apps from '../../shared/js/apps';
 import { formatDateTime } from '../../shared/js/date';
 import ContentWrapper from '../../shared/react-pure/ContentWrapper';
 import Divider from '../../shared/react-pure/Divider';
@@ -9,7 +8,6 @@ import Spacer from '../../shared/react-pure/Spacer';
 import AppBar from '../../shared/react/AppBar';
 import AppVersion from '../../shared/react/AppVersion';
 import ChangeTheme from '../../shared/react/ChangeTheme';
-import PaymentStatus from '../../shared/react/PaymentStatus';
 import RouteLink from '../../shared/react/RouteLink';
 
 function Home({ account, isLoadingAccount }) {
@@ -23,7 +21,6 @@ function Home({ account, isLoadingAccount }) {
             <Text margin="0 0 1rem">Username: {account.username}</Text>
             <Text margin="0 0 1rem">User ID: {account.userId}</Text>
             <Text margin="0 0 1rem">Created at: {formatDateTime(account.createdAt)}</Text>
-            <PaymentStatus app={apps.watcher37.name} showBuyButton />
             <Divider />
             <Spacer />
             {!!account.telegramId && (
