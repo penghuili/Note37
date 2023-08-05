6bf48e6a5c410624a29d2e962894e8d6c7fe1c0c
Peng <1a84@proton.me>
2023-08-05 08:12:24 +0200

    add source code link

--- src/views/Welcome/Welcome.js
+++ src/views/Welcome/Welcome.js
@@ -35,6 +35,8 @@ function Welcome() {
         <Spacer />
         <RouteLink label="Privacy" to="/privacy" />
         <Spacer />
+        <Anchor label="Source code" href="https://github.com/penghuili/Note37" target="_blank" />
+        <Spacer />
         <Anchor label="Contact" href="https://peng37.com/contact" target="_blank" />
 
         <Spacer />
