c13994c16426ce9a667fcded37732451c9764905
Peng <1a84@proton.me>
2023-08-17 15:42:57 +0200

    cache note value in local-storage

--- src/views/ItemAdd/ItemAdd.js
+++ src/views/ItemAdd/ItemAdd.js
@@ -6,9 +6,11 @@ import DatePicker from '../../shared/react-pure/DatePicker';
 import Spacer from '../../shared/react-pure/Spacer';
 import AppBar from '../../shared/react/AppBar';
 import TextEditor from '../../shared/react/TextEditor';
+import { LocalStorage } from '../../shared/js/LocalStorage';
 
+const storageKey = 'note37-item-add';
 function ItemAdd({ topicId, isCreating, onCreate }) {
-  const [note, setNote] = useState('');
+  const [note, setNote] = useState(LocalStorage.get(storageKey) || '');
   const [date, setDate] = useState(new Date());
 
   return (
@@ -17,13 +19,20 @@ function ItemAdd({ topicId, isCreating, onCreate }) {
       <ContentWrapper>
         <DatePicker label="Date" showTime value={date} onChange={setDate} />
         <Spacer />
-        <TextEditor text={note} onChange={setNote} />
+        <TextEditor
+          text={note}
+          onChange={value => {
+            setNote(value);
+            LocalStorage.set(storageKey, value);
+          }}
+        />
 
         <Spacer />
         <Button
           label="Create item"
           onClick={() => {
             onCreate({ id: topicId, note, date: new Date(date).getTime(), goBack: true });
+            LocalStorage.remove(storageKey);
           }}
           disabled={!date || isCreating}
         />
